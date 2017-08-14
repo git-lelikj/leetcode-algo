113a44dcb73f0f38f9ea24dd6618389d1994d01a
Alexei Jemkov <Alexei.Jemkov@advantechwireless.com>
2017-08-14 17:33:43 +0300

    New solutions

--- main.cpp
+++ main.cpp
@@ -1072,7 +1072,7 @@ int main(int argc, char** argv)
 //  ".Q.."]
 //]
 // -------------------------------------------------------------------------------------------------------
-
+#if 0
 #include <iostream>
 #include <vector>
 #include <ostream>
@@ -1290,10 +1290,78 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
+#endif
 
 // -------------------------------------------------------------------------------------------------------
-// 52. N-Queens II
-// Follow up for N-Queens problem.
-// Now, instead outputting board configurations, return the total number of distinct solutions.
+// 218. The Skyline Problem
+//    A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance.
+//    Now suppose you are given the locations and height of all the buildings as shown on a cityscape photo (Figure A),
+//    write a program to output the skyline formed by these buildings collectively (Figure B).
+//    The geometric information of each building is represented by a triplet of integers [Li, Ri, Hi], where Li and Ri are the x coordinates
+//    of the left and right edge of the ith building, respectively, and Hi is its height. It is guaranteed that
+//    0 ? Li, Ri ? INT_MAX, 0 < Hi ? INT_MAX, and Ri - Li > 0. You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+//    For instance, the dimensions of all buildings in Figure A are recorded as: [ [2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] ] .
+
+//    The output is a list of "key points" (red dots in Figure B) in the format of [ [x1,y1], [x2, y2], [x3, y3], ... ] that uniquely defines a skyline.
+//    A key point is the left endpoint of a horizontal line segment. Note that the last key point, where the rightmost building ends, is merely used to
+//    mark the termination of the skyline, and always has zero height. Also, the ground in between any two adjacent buildings should be considered part of the skyline contour.
+
+//    For instance, the skyline in Figure B should be represented as:[ [2 10], [3 15], [7 12], [12 0], [15 10], [20 8], [24, 0] ].
+
+//    Notes:
+
+//    The number of buildings in any input list is guaranteed to be in the range [0, 10000].
+//    The input list is already sorted in ascending order by the left x position Li.
+//    The output list must be sorted by the x position.
+//    There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable;
+//    the three lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
 // -------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
+        using Buildings_type = vector<vector<int>>;
+        using Skyline_type = vector<pair<int, int>>;
+
+        Skyline_type skyline;
+        if (buildings.size() == 0)
+            return skyline;
+        if (buildings.size() == 1) {
+            if (buildings[0].size() != 3)
+                return skyline;
+            skyline.emplace_back(buildings[0][0], buildings[0][2]);
+            skyline.emplace_back(buildings[0][1], 0);
+        }
+        // apply divide and conquier
+        // 1. divide
+        size_t median = buildings.size() / 2;
+        // 2. solve parts
+        Buildings_type buildings1(buildings.begin(), buildings.begin() + median);
+        Skyline_type  skyline1 = getSkyline(buildings1);
+    }
+};
+
+int main(int argc, char** argv)
+{
+    int in = (argc > 1 ? atoi(argv[1]) : 0);
+
+    Solution s;
+    vector<vector<int>> buildings = {
+        {2, 9, 10}
+    };
+
+    vector<pair<int, int>> skyline = s.getSkyline(buildings);
+
+    cout << "Result skyline size: " << skyline.size() << endl;
+    for (auto &dot: skyline) {
+        cout << "[" << dot.first << "," << dot.second << "] ";
+    }
+    cout << endl;
+
+    return 0;
+}
